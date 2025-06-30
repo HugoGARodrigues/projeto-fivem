c6430f4371605529b9139bb7448d6d05968f6168
Hugo Rodrigues <hugogabriel.es@gmail.com>
2025-06-30 06:46:11 -0300

    feature: alterando algumas coisas e subindo para sair de casa

--- src/main/java/com/example/sistemafivem_api/repository/AccessRepository.java
+++ src/main/java/com/example/sistemafivem_api/repository/AccessRepository.java
@@ -1,10 +1,13 @@
 package com.example.sistemafivem_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.sistemafivem_api.entity.Access;
 
 public interface AccessRepository extends JpaRepository<Access, Long> {
 
-    Access findByUserId(Integer userId);
+    Optional<Access> findByUserId(Integer userId);
+    Optional<Access> findByEmail(String email);
 }
--- src/main/java/com/example/sistemafivem_api/service/AcessService.java
+++ src/main/java/com/example/sistemafivem_api/service/AcessService.java
@@ -1,14 +1,14 @@
 package com.example.sistemafivem_api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import com.example.sistemafivem_api.repository.AccessRepository;
+
 import com.example.sistemafivem_api.entity.Access;
 import com.example.sistemafivem_api.entity.User;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.example.sistemafivem_api.repository.AccessRepository;
 
 @Service
 public class AcessService implements UserDetailsService {
@@ -17,14 +17,16 @@ public class AcessService implements UserDetailsService {
     private AccessRepository accessRepository;
 
     @Autowired
+    private Access access;
+
+    @Autowired
     private User user;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        user = user.findByEmail(email)
+        access = accessRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
-        Access access = accessRepository.findByUserId(user.id)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+       
         return access;
     }
     
